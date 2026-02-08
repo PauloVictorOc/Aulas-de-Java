d6077d3d030cb49e39b8ded422c56c15fe9408d0
viCtor <alphap2708@gmail.com>
2026-02-07 21:25:30 -0300

    Aulas de Arrays
    aula de introdução

---
+++ aulas/Aula07Arrays01.java
@@ -0,0 +1,13 @@
+package aulas;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args){
+        int[] idades = new int [3];
+        idades[0] = 12;
+        idades[1] = 45;
+        idades[2] = 32;
+        System.out.println(idades[0]);
+        System.out.println(idades[2]);
+        System.out.println(idades[1]);
+    }
+}
