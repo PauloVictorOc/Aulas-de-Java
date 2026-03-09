724ca7d984124f95d33011cf411f334bc99db03a
viCtor <alphap2708@gmail.com>
2026-03-09 18:59:46 -0300

    aula 1 de arrays dimencionais

---
+++ aulas/Aula08ArraysDimencionais01.java
@@ -0,0 +1,27 @@
+package aulas;
+
+public class Aula08ArraysDimencionais01 {
+    public static void main(String[] args){
+        /*
+        1,2,3,4,5 meses
+        31,28,31,30 dias
+         */
+        int[][] dias = new int[3][3];
+        dias[0][0] =31;
+        dias[0][1] =21;
+        dias[0][2] =18;
+
+        dias[1][0] = 31;
+        dias[1][1] = 21;
+        dias[1][2] = 18;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[0].length; j++) {
+                System.out.println(dias[i][j]);
+            }
+
+        }
+
+
+    }
+}
