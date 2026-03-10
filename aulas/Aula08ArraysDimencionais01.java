9b7a3d11c23ff9a4d359590347be4c85ffbb1ec5
viCtor <alphap2708@gmail.com>
2026-03-10 12:36:27 -0300

    arrays dimencionas

--- aulas/Aula08ArraysDimencionais01.java
+++ aulas/Aula08ArraysDimencionais01.java
@@ -16,12 +16,17 @@ public class Aula08ArraysDimencionais01 {
         dias[1][2] = 18;
 
         for (int i = 0; i < dias.length; i++) {
-            for (int j = 0; j < dias[0].length; j++) {
+            for (int j = 0; j < dias[i].length; j++) {
                 System.out.println(dias[i][j]);
             }
 
         }
-
+        System.out.println("----------------------------------------");
+    for (int[] arrBase:dias){
+        for (int num:arrBase){
+            System.out.println(num);
+        }
+    }
 
     }
 }
